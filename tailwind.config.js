51fed5bbc94e347179a59c59989125491ad0c01c
Jasper Stolwijk <jasperstolwijk@icloud.com>
2019-10-27 23:45:43 +0100

    Something went wrong idk what

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  theme: {
+    extend: {
+        colors: {
+            gray: {
+                bg: '#E4E4E4',
+            },
+            blue: {
+              main: '#008FA8',
+            }
+        }
+    }
+  },
+  variants: {},
+  plugins: []
+}
