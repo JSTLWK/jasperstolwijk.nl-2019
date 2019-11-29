53822cc58fc39577d0fc7c8ab5822a1df0f45e86
Jasper Stolwijk <jasperstolwijk@icloud.com>
2019-11-29 20:38:57 +0100

    Contact vue component

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,15 +18,31 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::middleware('throttle:30|60,1')->prefix('v1/portfolio')->group(function () {
+Route::middleware('throttle:30|60,1')->prefix('v1')->group(function () {
+
+
+    Route::post('/', function (Request $request) {
+
+        $destinationPath = 'ss';
+
+        $file = $request->file('sharex');
+        $random_string = Str::random(5);
+
+        $filename = $random_string.$file->getMimeType();
+
+        $file->moveAs($destinationPath, $filename);
+
+    })->name('sharex.api');
 
     /**
      * Route for the projects to show on the homepage.
      */
-    Route::resource('projects', 'Api\PortfolioController')->names([
-        'index' => 'api.projects'
-    ]);
-    Route::post('contact', 'Api\PortfolioController@sendEmail')->name('api.portfolio.contact');
+    Route::prefix('/portfolio')->group(function () {
+        Route::resource('projects', 'Api\PortfolioController')->names([
+            'index' => 'api.projects'
+        ]);
+        Route::post('contact', 'Api\PortfolioController@sendEmail')->name('api.portfolio.contact');
 
+    });
 });
 
