6b3118c5d9542f1333866099a54ea17e31fd4880
Jasper Stolwijk <jasperstolwijk@icloud.com>
2020-02-02 19:23:18 +0100

    JS-000: Projecten uitgebreid

--- resources/js/components/Portfolio/ProjectsFilter.vue
+++ resources/js/components/Portfolio/ProjectsFilter.vue
@@ -66,6 +66,29 @@
                         </div>
                         <span class="select-none">Statamic</span>
                     </label>
+                    <label class="custom-label items-center flex pl-4">
+                        <div class="bg-white shadow w-4 h-4 p-1 flex justify-center items-center mr-2">
+                            <input v-model="filters.tags" id="Prestashop" value="Prestashop" type="checkbox"
+                                   class="hidden">
+                            <svg class="hidden h-4 text-blue-main pointer-events-none"
+                                 viewBox="0 0 172 172">
+                                <g fill="none"
+                                   stroke-width="none"
+                                   stroke-miterlimit="10"
+                                   font-family="none"
+                                   font-weight="none"
+                                   font-size="none"
+                                   text-anchor="none"
+                                   style="mix-blend-mode:normal">
+                                    <path d="M0 172V0h172v172z"/>
+                                    <path d="M145.433 37.933L64.5 118.8658 33.7337 88.0996l-10.134 10.1341L64.5 139.1341l91.067-91.067z"
+                                          fill="currentColor"
+                                          stroke-width="1"/>
+                                </g>
+                            </svg>
+                        </div>
+                        <span class="select-none">Prestashop</span>
+                    </label>
                     <h3 class="pl-4 text-lg mt-2">Language</h3>
                     <label class="custom-label items-center flex pl-4">
                         <div class="bg-white shadow w-4 h-4 p-1 flex justify-center items-center mr-2">
