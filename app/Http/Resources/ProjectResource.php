b27e0a20fc8776cab49f82b28bb9be493f4c8bb0
Jasper Stolwijk <jasperstolwijk@icloud.com>
2020-02-02 20:00:13 +0100

    JS-000: Projecten uitgebreid -- image optimisation

--- app/Http/Resources/ProjectResource.php
+++ app/Http/Resources/ProjectResource.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use Spatie\LaravelImageOptimizer\Facades\ImageOptimizer;
 
 class ProjectResource extends JsonResource
 {
@@ -14,6 +15,13 @@ class ProjectResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'sorting_order' => $this->sorting_order,
+            'introduction' => ImageOptimizer::optimize($this->introduction),
+            'head_img' => $this->head_img,
+            'tags' => $this->tags,
+        ];
     }
 }
