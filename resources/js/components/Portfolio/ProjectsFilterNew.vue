f906039345d9c7a6948cd1bb288fa8e8c9ca57f2
Jasper Stolwijk <jasperstolwijk@icloud.com>
2020-06-18 04:55:36 +0200

    Fix Issue when searching for a project to show all projects first.

--- resources/js/components/Portfolio/ProjectsFilterNew.vue
+++ resources/js/components/Portfolio/ProjectsFilterNew.vue
@@ -17,8 +17,15 @@
         <div class="hidden sm:flex flex-wrap sm:-mt-24">
             <div class="flex mr-24 overflow-hidden relative w-full">
                 <div class="z-10 relative text-white flex items-center text-white justify-center  text-white">
-                    <svg width="300px" xmlns="http://www.w3.org/2000/svg" class="ml-1 mt-1 mt-2 w-2/4" viewBox="0 0 148.856 170.886">
-                        <path id="Path_1" data-name="Path 1" d="M40.5-76.3c8.3,10.9,8.9,29.2,13.3,43.1,4.4,14,12.6,23.6,14.6,34.3,2,10.8-2.3,22.7-8.9,32.9C53,44.2,44.1,52.7,33.8,60.8c-10.3,8.2-22,16-35.9,19.6-13.8,3.6-29.8,3-37.7-6.5-7.9-9.6-7.8-28-14.6-41.8S-74.9,9.1-78.8-2.2c-3.8-11.4,2.2-25,11-35.3C-59-47.9-47.4-55-35.7-63.9c11.8-8.9,23.8-19.6,37.8-23.2S32.2-87.1,40.5-76.3Z" transform="translate(79.962 88.45)" fill="#4299e1"/>
+                    <svg width="300px"
+                         xmlns="http://www.w3.org/2000/svg"
+                         class="ml-1 mt-1 mt-2 w-2/4"
+                         viewBox="0 0 148.856 170.886">
+                        <path id="Path_1"
+                              data-name="Path 1"
+                              d="M40.5-76.3c8.3,10.9,8.9,29.2,13.3,43.1,4.4,14,12.6,23.6,14.6,34.3,2,10.8-2.3,22.7-8.9,32.9C53,44.2,44.1,52.7,33.8,60.8c-10.3,8.2-22,16-35.9,19.6-13.8,3.6-29.8,3-37.7-6.5-7.9-9.6-7.8-28-14.6-41.8S-74.9,9.1-78.8-2.2c-3.8-11.4,2.2-25,11-35.3C-59-47.9-47.4-55-35.7-63.9c11.8-8.9,23.8-19.6,37.8-23.2S32.2-87.1,40.5-76.3Z"
+                              transform="translate(79.962 88.45)"
+                              fill="#4299e1"/>
                     </svg>
 
 
@@ -34,7 +41,9 @@
                             <label class="custom-label flex z-10 my-1">
                                 <div class="bg-white shadow w-6 h-6 p-1 flex justify-center items-center mr-2 z-10">
                                     <input type="checkbox"
-                                           v-model="filters.tags" id="Statamic" value="Statamic"
+                                           v-model="filters.tags"
+                                           id="Statamic"
+                                           value="Statamic"
                                            class="hidden"
                                            checked>
                                     <svg class="hidden w-4 h-4 text-green-600 pointer-events-none z-10"
@@ -60,7 +69,9 @@
                                 <div class="bg-white shadow w-6 h-6 p-1 flex justify-center items-center mr-2 z-10">
                                     <input type="checkbox"
                                            class="hidden"
-                                           v-model="filters.tags" id="Prestashop" value="Prestashop"
+                                           v-model="filters.tags"
+                                           id="Prestashop"
+                                           value="Prestashop"
                                            checked>
                                     <svg class="hidden w-4 h-4 text-green-600 pointer-events-none z-10"
                                          viewBox="0 0 172 172">
@@ -84,7 +95,9 @@
                             <label class="custom-label flex z-10 my-1">
                                 <div class="bg-white shadow w-6 h-6 p-1 flex justify-center items-center mr-2 z-10">
                                     <input type="checkbox"
-                                           v-model="filters.tags" id="Laravel" value="Laravel"
+                                           v-model="filters.tags"
+                                           id="Laravel"
+                                           value="Laravel"
                                            class="hidden"
                                            checked>
                                     <svg class="hidden w-4 h-4 text-green-600 pointer-events-none z-10"
@@ -113,7 +126,9 @@
                             <label class="custom-label flex z-10 my-1">
                                 <div class="bg-white shadow w-6 h-6 p-1 flex justify-center items-center mr-2 z-10">
                                     <input type="checkbox"
-                                           v-model="filters.tags" id="PHP" value="PHP"
+                                           v-model="filters.tags"
+                                           id="PHP"
+                                           value="PHP"
                                            class="hidden"
                                            checked>
                                     <svg class="hidden w-4 h-4 text-green-600 pointer-events-none z-10"
@@ -138,7 +153,8 @@
                             <label class="custom-label flex z-10 my-1">
                                 <div class="bg-white shadow w-6 h-6 p-1 flex justify-center items-center mr-2 z-10">
                                     <input type="checkbox"
-                                           v-model="filters.tags" value="VueJS"
+                                           v-model="filters.tags"
+                                           value="VueJS"
                                            class="hidden"
                                            checked>
                                     <svg class="hidden w-4 h-4 text-green-600 pointer-events-none z-10"
@@ -163,7 +179,8 @@
                             <label class="custom-label flex z-10 my-1">
                                 <div class="bg-white shadow w-6 h-6 p-1 flex justify-center items-center mr-2 z-10">
                                     <input type="checkbox"
-                                           v-model="filters.tags" value="Javascript"
+                                           v-model="filters.tags"
+                                           value="Javascript"
                                            class="hidden"
                                            checked>
                                     <svg class="hidden w-4 h-4 text-green-600 pointer-events-none z-10"
@@ -207,14 +224,16 @@
         <!-- Results -->
 
         <div class="container mx-auto -mt-8 sm:mt-8 flex flex-wrap">
-            <div v-for="project in projects" class="w-full sm:w-1/3 px-2">
+            <div v-for="project in projects"
+                 class="w-full sm:w-1/3 px-2">
                 <a href=""
                    title="">
                     <div class="flex flex-col justify-center items-center max-w-sm mx-auto my-4">
                         <div :style="{ backgroundImage: `url('${project.head_img}')` }"
                              class="bg-gray-300 h-64 w-full rounded-lg shadow-md bg-cover bg-center"></div>
                         <div class="w-56 md:w-64 bg-white -mt-32 shadow-lg rounded-lg overflow-hidden">
-                            <div class="py-2 text-center font-bold uppercase tracking-wide text-gray-800">{{ project.title}}
+                            <div class="py-2 text-center font-bold uppercase tracking-wide text-gray-800">{{
+                                project.title}}
                             </div>
                             <div class="flex h-32 px-3 bg-blue-200">
                                 <p class="text-blue-800 text-center text-sm font-bold pt-1">
@@ -246,7 +265,6 @@
                 },
                 title: "",
                 projects: "",
-                url: this.endpoint
             }
         },
         watch: {
@@ -254,13 +272,10 @@
                 handler(newValue, oldValue) {
 
                     if (newValue.tags.length > 0) {
-                        this.url = this.endpoint + "?tags=" + newValue.tags;
+                        let url = this.endpoint + "?tags=" + newValue.tags;
 
                         this.title = "";
-                        this.getProjects();
-                    } else {
-                        this.url = this.endpoint;
-                        this.getProjects();
+                        this.getProjects(url);
                     }
 
                 },
@@ -269,26 +284,24 @@
             title: {
                 handler(newValue, oldValue) {
                     this.filters.tags = [];
-                    this.url          = this.endpoint + "?title=" + newValue;
+                    let url           = this.endpoint + "?title=" + newValue;
 
-                    this.getProjects();
+                    this.getProjects(url);
                 }
             }
         },
         created() {
-            this.getProjects();
+            this.getProjects(this.endpoint);
         },
         methods: {
-            getProjects() {
-
+            getProjects(url) {
                 let self = this;
 
-                axios.get(this.url)
+                axios.get(url)
                      .then(function(response) {
                          self.projects = response.data;
                      })
                      .catch(function(error) {
-                         // handle error
                          console.log(error);
                      });
             }
