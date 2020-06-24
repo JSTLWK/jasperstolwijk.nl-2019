84843e35da2b074d24a22c147caf92218d661890
Jasper Stolwijk <jasperstolwijk@icloud.com>
2020-06-25 00:49:52 +0200

    Smooth scroll & Error Handling slack

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,7 +3,9 @@
 namespace App\Exceptions;
 
 use Exception;
+use GuzzleHttp\Client;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Support\Facades\App;
 
 class Handler extends ExceptionHandler
 {
@@ -34,6 +36,53 @@ class Handler extends ExceptionHandler
      */
     public function report(Exception $exception)
     {
+        if (App::environment('production')) {
+            $message = [
+                "attachments" => [
+                    [
+                        "color"  => "#F56565",
+                        "blocks" => [
+                            [
+                                "type" => "section",
+                                "text" => [
+                                    "type" => "mrkdwn",
+                                    "text" => "An error has been found on the domain: *<JasperStolwijk.nl|JasperStolwijk.nl>*",
+                                ],
+                            ],
+                            [
+                                "type"   => "section",
+                                "fields" => [
+                                    [
+                                        "type" => "mrkdwn",
+                                        "text" => "*Message:*
+internal server error",
+                                    ],
+                                    [
+                                        "type" => "mrkdwn",
+                                        "text" => "*Error:*
+500",
+                                    ],
+                                ],
+                            ],
+                        ],
+                    ],
+                ],
+            ];
+
+            $domain = getenv('APP_URL');
+
+            $slackUrI     = "https://hooks.slack.com/services/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";
+            $Errormessage = "An error has been found on the domain: *<{$domain}|{$domain}>*";
+
+            $message['attachments'][0]['blocks'][0]['text']['text']      = $Errormessage;
+            $message['attachments'][0]['blocks'][1]['fields'][0]['text'] = "*Message:* \n" . $exception->getMessage();
+            $message['attachments'][0]['blocks'][1]['fields'][1]['text'] = "*Error:* \n" . $exception->getCode();
+
+            // Send to slack channel
+            $client = new Client();
+            $client->post($slackUrI, ['body' => json_encode($message)]);
+        }
+
         parent::report($exception);
     }
 
