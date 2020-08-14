22d23cd7a1e3e3725541020c09b19f58a8ef7726
Jasper Stolwijk <jasperstolwijk@icloud.com>
2020-08-15 00:11:10 +0200

    Downgrading from laravel 7.x to 6.2

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -29,11 +29,10 @@ class Handler extends ExceptionHandler
     /**
      * Report or log an exception.
      *
-     * @param \Throwable $exception
+     * @param  \Exception  $exception
      * @return void
-     * @throws Exception
      */
-    public function report(\Throwable $exception)
+    public function report(Exception $exception)
     {
         parent::report($exception);
     }
