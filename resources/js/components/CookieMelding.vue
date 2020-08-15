9b167006ffe427cd4e804ced4cdc1fb05461f2db
Jasper Stolwijk <jasperstolwijk@icloud.com>
2020-08-15 20:12:32 +0200

    Google Analytics on the website - Translation

--- resources/js/components/CookieMelding.vue
+++ resources/js/components/CookieMelding.vue
@@ -1,19 +1,17 @@
 <template>
     <div v-show="! closeCookie" class="bg-blue-500 bottom-0 fixed mx-5 sm:mx-10 my-5 px-2 py-5 right-0 text-white sm:w-2/5">
-        <h2 class="px-4 font-bold text-xl">Wil jij ook een goed werkende site?</h2>
-        <p class="px-4 py-2 text-sm">Mijn website gebruikt cookies om content en advertenties te personaliseren, om functies voor sociale media te
-            bieden en om ons website verkeer te analyseren. Ook delen we informatie over uw gebruik van mijn site met
-            onze partners voor social media, adverteren en analyse.
+        <h2 class="px-4 font-bold text-xl">Do you like a good working site?</h2>
+        <p class="px-4 py-2 text-sm">My Website uses cookies to personalize content and advertisements and to analyze our website traffic.
         </p>
         <div class="flex flex-wrap">
             <div class="w-1/2 px-2">
                 <div @click="acceptCookie" class="rounded-lg cursor-pointer py-4 text-blue-500 bg-white flex justify-center items-center">
-                    Accepteren
+                    Accept
                 </div>
             </div>
             <div class="w-1/2 px-2">
                 <div class="h-full flex justify-center items-center">
-                    <a href="#">Lees meer</a>
+                    <a href="https://cookie-consent.app.forthe.top/why-websites-use-cookies/" target="_blank">Read more</a>
                 </div>
             </div>
         </div>
